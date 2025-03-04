5135bccdf8290d65158caa26618a64344bd44558
Anshik Jaiswal <anshik0706@gmail.com>
2025-03-04 23:33:14 +0530

    [Anshik] Add. using validation in attributes

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,7 +1,7 @@
-class Contact{
-    constructor(firstName, lastNames, address, city, state, zip, phoneNumber, email){
+class Contact {
+    constructor(firstName, lastName, address, city, state, zip, phoneNumber, email) {
         this.firstName = firstName;
-        this.lastNames = lastNames;
+        this.lastName = lastName;
         this.address = address;
         this.city = city;
         this.state = state;
@@ -12,3 +12,30 @@ class Contact{
 }
 
 let addressBook = [];
+
+function validate(contact) {
+    const nameRegex = /^[A-Z][a-z]{2,}/;
+    const addressRegex = /^.{4,}/;
+    const zipRegex = /^\d{5}$/;
+    const phoneRegex = /^\d{10}$/;
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+    if (!nameRegex.test(contact.firstName) || !nameRegex.test(contact.lastName)) {
+        throw "Invalid Name";
+    }
+    if (
+        !addressRegex.test(contact.address) ||
+        !addressRegex.test(contact.city) ||
+        !addressRegex.test(contact.state)
+    ) {
+        throw "Invalid Address";
+    }
+    if (
+        !zipRegex.test(contact.zip) ||
+        !phoneRegex.test(contact.phoneNumber) ||
+        !emailRegex.test(contact.email)
+    ) {
+        throw "Invalid Contact Information";
+    }
+    return true;
+}
