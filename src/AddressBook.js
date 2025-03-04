6919ceceb6f54bf135f66f39867ea9687fa620a6
Anshik Jaiswal <anshik0706@gmail.com>
2025-03-04 16:00:49 +0530

    [Anshik] Add. created addressBook

---
+++ src/AddressBook.js
@@ -0,0 +1,14 @@
+class Contact{
+    constructor(firstName, lastNames, address, city, state, zip, phoneNumber, email){
+        this.firstName = firstName;
+        this.lastNames = lastNames;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+}
+
+let addressBook = [];
